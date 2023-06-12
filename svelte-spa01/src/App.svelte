f87d495b72da41e6244048604f0e4e44fb828551
Ramirez Vargas, José Pablo <webJose@gmail.com>
2023-06-11 22:37:44 -0600

    WIP

--- svelte-spa01/src/App.svelte
+++ svelte-spa01/src/App.svelte
@@ -28,19 +28,16 @@
     align-items: center;
     padding: 1em;
   }
-  .logo {
+  img.logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
   }
-  .logo:hover {
+  img.logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
-  .logo.svelte:hover {
+  img.logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
--- svelte-spa01/vite.config.ts
+++ svelte-spa01/vite.config.ts
@@ -1,42 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import vitePluginSingleSpa from 'vite-plugin-single-spa';
 
 export default function (opts) {
-  console.log('Executing Vite %s in %s mode...', opts.command, opts.mode);
-  const input = {};
-  let preserveEntrySignatures: false | 'strict' | 'allow-extension' | 'exports-only';
-  const assetFileNames = 'assets/[name][extname]';
-  const entryFileNames = '[name].js';
-  if (opts.mode === 'standalone') {
-    input['index'] = 'index.html';
-    preserveEntrySignatures = false;
-  }
-  else {
-    console.log('SPA build.');
-    input['spa'] = 'src/spa.ts';
-    preserveEntrySignatures = 'exports-only';
-  }
   return defineConfig({
-    plugins: [svelte()],
-    build: {
-      target: 'es2022',
-      manifest: true,
-      rollupOptions: {
-        input,
-        preserveEntrySignatures,
-        output: {
-          exports: 'auto',
-          assetFileNames,
-          entryFileNames
-        }
-      }
-    },
-    base: 'http://localhost:4101',
-    server: {
-      port: 4101
-    },
-    preview: {
-      port: 4101
-    }
+    plugins: [svelte(), vitePluginSingleSpa({
+      type: 'mife',
+      serverPort: 4101
+    })],
   });
 };
